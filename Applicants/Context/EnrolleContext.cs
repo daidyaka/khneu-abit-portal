00000faf9041a17ec7d1b04cd1ce8c177e1dd937
AlexMyrosh <miron7161@gmail.com>
2022-02-08 11:59:57 +0200

    switch aspnet 6 to aspnet 5. Design changes

---
+++ Applicants/Context/EnrolleContext.cs
@@ -0,0 +1,15 @@
+﻿using Enrollee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Enrollee.Context
+{
+    public class EnrolleContext:DbContext
+    {
+        public DbSet<Feedback> Feedbacks { get; set; }
+        public EnrolleContext(DbContextOptions<EnrolleContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
--- Enrollee/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Enrollee.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
