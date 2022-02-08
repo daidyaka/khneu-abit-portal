d9b4bb75b3ed5b59622126d694e449430c07b15f
AlexMyrosh <miron7161@gmail.com>
2022-02-08 11:59:57 +0200

    switch aspnet 6 to aspnet 5. Design changes

---
+++ Applicants/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Enrollee.Context;
+using Enrollee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Enrollee.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly EnrolleContext _context;
+        public HomeController(EnrolleContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SendRequest(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
